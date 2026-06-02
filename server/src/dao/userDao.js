f1327bc622d819b1664d3f02e5e8d64126ed00f9
wahe7 <wahegurusingh2002@gmail.com>
2026-06-03 02:51:41 +0530

    create dao for user

---
+++ server/src/dao/userDao.js
@@ -0,0 +1,12 @@
+export function toUserProfile(user) {
+  return {
+    login: user.login,
+    avatarUrl: user.avatar_url,
+    name: user.name,
+    bio: user.bio,
+    followers: user.followers,
+    following: user.following,
+    publicRepos: user.public_repos,
+    htmlUrl: user.html_url,
+  };
+}
