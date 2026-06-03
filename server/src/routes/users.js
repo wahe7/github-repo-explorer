5e0dc2bc91ea4f096ec6ddc6d3e636a36470c64c
wahe7 <wahegurusingh2002@gmail.com>
2026-06-04 00:28:45 +0530

    add new route for repo stream

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -3,6 +3,7 @@ import * as userController from '../controllers/userController.js';
 
 const router = Router();
 
+router.get('/:username/repos/stream', userController.getReposStream);
 router.get('/:username/repos/:repoName', userController.getRepoDetail);
 router.get('/:username/repos', userController.getRepos);
 router.get('/:username', userController.getProfile);
